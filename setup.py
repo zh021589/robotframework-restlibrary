7eb5fe75e7571756a20cf357227dafed3e792665
lindstream <lindstream@b20b0688-c145-11dd-84b3-df5d23adf0f1>
2008-12-03 15:17:12 +0000

    Initial code base.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+from distutils.core import setup
+from RestLibrary import __version__
+
+setup(
+    name='RestLibrary',
+    version=__version__,
+    py_modules=['RestLibrary'],
+    #install_requires = [
+    #    'httplib2',
+    #    'lxml'
+    #],
+)
+
